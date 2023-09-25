3eb1213fdefec665546852a888021f271ebfc472
EA - João Vitor Silva De Araujo <vitor_araujo@educadventista.org>
2023-09-25 14:54:57 -0300

    recapitulação de render html

--- 2_EXPRESS/5_recap/index.js
+++ 2_EXPRESS/5_recap/index.js
@@ -1,8 +1,11 @@
 const express = require('express')
+const path = require("path")
 const app = express()
 
+const caminho = path.join(__dirname,"templates")
+
 app.get('/', (requisicao,resposta) =>{
-    resposta.send('seja bem vindo ao meu servidor')
+    resposta.sendFile(`${caminho}/index.html`)
 })
 
 app.listen (3000,() =>{
